cfa698cc5a05ae6783eea718b7958e7a978e14e9
chuxwa <799586940@qq.com>
2022-03-17 19:38:20 +0800

    init Open3DVisualizer

--- visualization/open3d_methods.py
+++ visualization/open3d_methods.py
@@ -1,16 +1,137 @@
 import numpy as np
 import open3d as o3d
 
-def PointCloudVisualize(points, colors):
-    points,colors
-    point_cloud = o3d.geometry.PointCloud()
-    point_cloud.points = o3d.utility.Vector3dVector(points)
-    point_cloud.colors = o3d.utility.Vector3dVector(colors)
-    o3d.visualization.draw_geometries([point_cloud])
 
+class Open3DVisualizer(object):
+    """Point cloud visualizer by using Open3D
 
-def DepthFilter(points_3d,colors,depth):
-    flag = (depth>0) & (depth<45)
-    real_points_3d = points_3d[flag,:]
-    real_colors = colors[flag,:]
-    return real_points_3d, real_colors
+    Args:
+        name (str, optional): the name of the visualization window. Defaults to "stereo reconstruction".
+        width (int, optional): the width of the visualization window. Defaults to 800.
+        height (int, optional): the height of the visualization window. Defaults to 600.
+        point_size (int, optional): the size of the points. Defaults to 1.
+        filename (str, optional): the filename of the viewpoint. Defaults to "./config/view_point.json".
+    """
+
+    def __init__(
+        self,
+        name: str = "stereo reconstruction",
+        width: int = 800,
+        height: int = 600,
+        point_size: int = 2,
+        filename: str = "output/config/visualization/view_point.json",
+    ):
+        """Point cloud visualizer by using Open3D
+
+        Args:
+            name (str, optional): the name of the visualization window. Defaults to "stereo reconstruction".
+            width (int, optional): the width of the visualization window. Defaults to 800.
+            height (int, optional): the height of the visualization window. Defaults to 600.
+            point_size (int, optional): the size of the points. Defaults to 1.
+            filename (str, optional): the filename of the viewpoint. Defaults to "./config/view_point.json".
+        """
+        self.name = name
+        self.width = width
+        self.height = height
+        # init the visualization window
+        self.visualizer = o3d.visualization.Visualizer()
+        self.visualizer.create_window(window_name=name, width=width, height=height)
+
+        # init the visualization parameter
+        self.opt = self.visualizer.get_render_option()
+        self.opt.background_color = np.asarray([1, 1, 1])
+        self.opt.point_size = point_size
+        self.opt.show_coordinate_frame = True
+
+        # load viewpoint
+        self.LoadViewPoint(filename)
+        # self.param = o3d.io.read_pinhole_camera_parameters(filename)
+        # self.ctr = self.visualizer.get_view_control()
+
+        # init the point cloud
+        self.pointcloud = o3d.geometry.PointCloud()
+        self.visualizer.add_geometry(self.pointcloud)
+
+    def __del__(self):
+        """Del the window."""
+        self.visualizer.destroy_window()
+
+    def UpdatePointCloud(self, points: np.ndarray, colors: np.ndarray):
+        """Update the point cloud
+
+        Args:
+            points (np.ndarray): the positions of the points
+            colors (np.ndarray): the colors of the points
+        """
+        self.pointcloud.points = o3d.utility.Vector3dVector(points)
+        self.pointcloud.colors = o3d.utility.Vector3dVector(colors)
+
+        self.visualizer.clear_geometries()  # clear
+        self.visualizer.add_geometry(self.pointcloud)  # add
+        # self.visualizer.update_geometry(self.pointcloud)  # update
+        # self.vis.remove_geometry(self.pointcloud)          # remove
+
+        # set viewpoint
+        self.ctr.convert_from_pinhole_camera_parameters(self.param)
+        self.visualizer.run()
+        self.visualizer.poll_events()
+        self.visualizer.update_renderer()
+
+    def CaptureScreen(self, filename: str, depth: bool = False):
+        """Capture the visualization screen
+
+        Args:
+            filename (str): the path of saving image
+            depth (bool, optional): capture depth map or full screen. Defaults to False.
+        """
+        if depth:
+            self.visualizer.capture_depth_image(filename, False)
+        else:
+            self.visualizer.capture_screen_image(filename, False)
+
+    def SaveViewPoint(self, filename: str):
+        """Save view point of the visualization screen
+
+        Args:
+            filename (str): the path of saving view point
+        """
+        self.visualizer.run()  # user changes the view and press "q" to terminate
+        self.param = self.ctr.convert_to_pinhole_camera_parameters()
+        o3d.io.write_pinhole_camera_parameters(filename, self.param)
+        self.visualizer.destroy_window()
+
+    def LoadViewPoint(self, filename: str):
+        """Load view point of the visualization screen
+
+        Args:
+            filename (str): the path of loading view point
+        """
+        self.param = o3d.io.read_pinhole_camera_parameters(filename)
+        self.ctr = self.visualizer.get_view_control()
+        self.ctr.convert_from_pinhole_camera_parameters(self.param)
+
+    def custom_draw_geometry_with_key_callback(self):
+        def rotate_view_w():
+            self.ctr.rotate(10.0, 0.0)
+            return False
+
+        def rotate_view_s():
+            self.ctr.rotate(10.0, 0.0)
+            return False
+
+        def rotate_view_a():
+            self.ctr.rotate(10.0, 0.0)
+            return False
+
+        def rotate_view_d():
+            self.ctr.rotate(0.0, 10.0)
+            return False
+
+        key_to_callback = {}
+        key_to_callback[ord("w")] = rotate_view_w
+        key_to_callback[ord("s")] = rotate_view_s
+        key_to_callback[ord("a")] = rotate_view_a
+        key_to_callback[ord("d")] = rotate_view_d
+        o3d.visualization.draw_geometries_with_key_callbacks(
+            [self.pointcloud], key_to_callback
+        )
